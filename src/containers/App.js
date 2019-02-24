150d5c66444fb3db710cbb1afab8cee5cbeceaa9
Patrick McCaffrey <patrick.mccaffrey@accordo.com>
2019-02-24 19:16:02 +1300

    basic page layout and running app

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,13 +5,15 @@ import styled from 'styled-components';
 import {Table} from './Table';
 
 const Body = styled.div`
-  display: 'flex'; 
-  flexDirection: 'column'; 
-  padding: '50px';
-  background-color: blue;
-`;
+  margin: 0;
+  padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: 'flex';
 
-const Header = styled.header`
   background-color: #282c34;
   min-height: 100vh;
   display: flex;
@@ -22,18 +24,47 @@ const Header = styled.header`
   color: white;
 `;
 
+const TitleBox = styled.div`
+  padding: 20px;
+  border-radius: 7px;
+  background-color: blue;
+  filter: drop-shadow(30px 10px 4px #4444dd);
+  filter: drop-shadow(30px 10px 4px #4444dd);
+  margin-bottom: 50px;
+`;
+
+const Header = styled.header``;
+
+const ContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  height: 100vh;
+  width: 100vh;
+  padding: 60px 80px;
+`;
+
+const H2 = styled.h2`
+  margin: 0;
+`;
+
 const App = () => {
   return (
     <div className="App">
       <Header>
         <Body>
-          <h2> refactionjsfe </h2>
-          <Table />
+          <ContentBox>
+            <TitleBox>
+              <H2> refactionjsfe </H2>
+            </TitleBox>
+            <Table />
+          </ContentBox>
         </Body>
       </Header>
     </div>
   );
-}
+};
 
 const mapStateToProps = state => ({
   dots: state.pixels,
--- src/containers/Table.js
+++ src/containers/Table.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-export function Table({ ...props }) {
+const CodeBlock = styled.pre`
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+  font-size: 12px;
+`;
+
+const H2 = styled.h2`
+  text-align: center;
+`;
+
+export function Table({...props}) {
   return (
     <div>
-      
+      <H2>
+        Table
+      </H2>
+      <CodeBlock>
+        {`
+            {
+              {name: 'Jim', age: 30, gender: 'male', _id: 'b3Fshn8F976TZCTg'},
+              {name: 'Jane', age: 55, gender: 'female', _id: 'k3nEqkqlKmWZNejC'},
+              {name: 'Bob', age: 20, gender: 'male', _id: 'oqnu2ZnPTebp04bG'},
+              {name: 'Sally', age: 24, gender: 'female', _id: 'tKmv8RC6GlUnYcV3'})
+            }
+          `}
+      </CodeBlock>
     </div>
   );
 }
 
 Table.defaultProps = {};
 
-Table.propTypes = {
-};
+Table.propTypes = {};
 
 export default Table;
